7ae8dafa825b06b6f9c31808c58418154f8f588c
capugafl <capugafl@ittepic.edu.mx>
2018-05-07 12:41:12 -0600

    4.3 - Graficos 2D

---
+++ 4.3-Graficos2D/app/src/main/java/com/example/puga/a43_relojanalogico/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.puga.a43_relojanalogico;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
